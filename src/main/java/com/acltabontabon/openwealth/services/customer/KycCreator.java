3e8e2462cc9f1d21436e35970a4d7f5bfdca52e3
Alvin Cris Tabontabon <me@acltabontabon.com>
2024-12-23 22:36:19 +0800

    Capability to create a new kyc object for a specific customer (#37)

--- src/main/java/com/acltabontabon/openwealth/services/customer/KycCreator.java
+++ src/main/java/com/acltabontabon/openwealth/services/customer/KycCreator.java
@@ -3,15 +3,14 @@ package com.acltabontabon.openwealth.services.customer;
 import static com.acltabontabon.openwealth.configs.Constants.HEADER_CORRELATION_ID;
 
 import com.acltabontabon.openwealth.configs.OpenWealthApiProperties;
+import com.acltabontabon.openwealth.dtos.CustomerApiResponse;
 import com.acltabontabon.openwealth.models.Kyc;
-import com.acltabontabon.openwealth.services.QueryAsyncCommand;
-import java.util.List;
+import com.acltabontabon.openwealth.services.CreateAsyncCommand;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.web.client.RestClient;
 
 @RequiredArgsConstructor
-public class PersonKycQuery extends QueryAsyncCommand<List<Kyc>> {
+public class KycCreator extends CreateAsyncCommand<CustomerApiResponse> {
 
     private final RestClient restClient;
     private final OpenWealthApiProperties.CustomerManagement apiProperties;
@@ -20,16 +19,19 @@ public class PersonKycQuery extends QueryAsyncCommand<List<Kyc>> {
     private final String personId;
     private final String correlationId;
 
+    private final Kyc kycToAdd;
+
     @Override
-    protected List<Kyc> execute() {
+    protected CustomerApiResponse execute() {
         try {
-            return restClient.get()
+            return restClient.post()
                 .uri(builder -> builder.path(apiProperties.getPersonKyc()).build(this.customerId, this.personId))
                 .header(HEADER_CORRELATION_ID, this.correlationId)
+                .body(kycToAdd)
                 .retrieve()
-                .body(new ParameterizedTypeReference<>() {});
+                .body(CustomerApiResponse.class);
         } catch (Exception e) {
-            throw new RuntimeException("Failed to fetch person details",e);
+            throw new RuntimeException("Failed to create KYC details", e);
         }
     }
 }
