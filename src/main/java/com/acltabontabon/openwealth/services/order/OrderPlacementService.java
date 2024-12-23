4063ddfba4b2fc47c714e2d3d6592ec2f778828a
Alvin Cris Tabontabon <me@acltabontabon.com>
2024-12-23 23:11:00 +0800

    Refactor customer preCheck

---
+++ src/main/java/com/acltabontabon/openwealth/services/order/OrderPlacementService.java
@@ -0,0 +1,10 @@
+package com.acltabontabon.openwealth.services.order;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class OrderPlacementService {
+
+}
